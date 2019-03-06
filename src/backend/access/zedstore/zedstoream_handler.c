df1db2fa05bae18cd11198deb39348486aeb2832
Ashwin Agrawal <aagrawal@pivotal.io>
2019-03-05 17:29:15 -0800

    ZedStore: Lets get started, initial AM code.
    With this can perform,
    create table t_zedstore(c1 int, c2 int) USING zedstore;
    insert into t_zedstore select i,i from generate_series(1, 10)i;
    
    insert and create code is currently using the heapam methods, just to
    start with. All other AM interfaces just ERROR out as not implemented
    yet.

---
+++ src/backend/access/zedstore/zedstoream_handler.c
@@ -0,0 +1,375 @@
+/*-------------------------------------------------------------------------
+ *
+ * zedstoream_handler.c
+ *	  heap table access method code
+ *
+ * Portions Copyright (c) 1996-2017, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *	  src/backend/access/zedstore/zedstoream_handler.c
+ *
+ *
+ * NOTES
+ *	  This file contains the zedstore_ routines which implement
+ *	  the POSTGRES zedstore table access method used for all POSTGRES
+ *	  relations.
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+
+#include <math.h>
+
+#include "miscadmin.h"
+
+#include "access/heapam.h"
+#include "access/relscan.h"
+#include "access/tableam.h"
+#include "catalog/catalog.h"
+#include "catalog/index.h"
+#include "catalog/pg_am_d.h"
+#include "catalog/storage.h"
+#include "catalog/storage_xlog.h"
+#include "utils/builtins.h"
+#include "utils/rel.h"
+#include "storage/bufmgr.h"
+
+
+/* ----------------------------------------------------------------
+ *				storage AM support routines for zedstoream
+ * ----------------------------------------------------------------
+ */
+
+static bool
+zedstoream_fetch_row_version(Relation relation,
+						 ItemPointer tid,
+						 Snapshot snapshot,
+						 TupleTableSlot *slot,
+						 Relation stats_relation)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+	return false;
+}
+
+/*
+ * Insert a heap tuple from a slot, which may contain an OID and speculative
+ * insertion token.
+ */
+static void
+zedstoream_insert(Relation relation, TupleTableSlot *slot, CommandId cid,
+				   int options, BulkInsertState bistate)
+{
+	bool		shouldFree = true;
+	HeapTuple	tuple = ExecFetchSlotHeapTuple(slot, true, &shouldFree);
+
+	/* Update the tuple with table oid */
+	slot->tts_tableOid = RelationGetRelid(relation);
+	if (slot->tts_tableOid != InvalidOid)
+		tuple->t_tableOid = slot->tts_tableOid;
+
+	/* Perform the insertion, and copy the resulting ItemPointer */
+	heap_insert(relation, tuple, cid, options, bistate);
+	ItemPointerCopy(&tuple->t_self, &slot->tts_tid);
+
+	if (shouldFree)
+		pfree(tuple);
+}
+
+static void
+zedstoream_insert_speculative(Relation relation, TupleTableSlot *slot, CommandId cid,
+							   int options, BulkInsertState bistate, uint32 specToken)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static void
+zedstoream_complete_speculative(Relation relation, TupleTableSlot *slot, uint32 spekToken,
+								 bool succeeded)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+
+static HTSU_Result
+zedstoream_delete(Relation relation, ItemPointer tid, CommandId cid,
+				   Snapshot snapshot, Snapshot crosscheck, bool wait,
+				   HeapUpdateFailureData *hufd, bool changingPart)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+
+static HTSU_Result
+zedstoream_lock_tuple(Relation relation, ItemPointer tid, Snapshot snapshot,
+				TupleTableSlot *slot, CommandId cid, LockTupleMode mode,
+				LockWaitPolicy wait_policy, uint8 flags,
+				HeapUpdateFailureData *hufd)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+
+static HTSU_Result
+zedstoream_update(Relation relation, ItemPointer otid, TupleTableSlot *slot,
+				   CommandId cid, Snapshot snapshot, Snapshot crosscheck,
+				   bool wait, HeapUpdateFailureData *hufd,
+				   LockTupleMode *lockmode, bool *update_indexes)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static void
+zedstoream_finish_bulk_insert(Relation relation, int options)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static const TupleTableSlotOps *
+zedstoream_slot_callbacks(Relation relation)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+
+	return &TTSOpsBufferHeapTuple;
+}
+
+static bool
+zedstoream_tuple_satisfies_snapshot(Relation rel, TupleTableSlot *slot, Snapshot snapshot)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static IndexFetchTableData*
+zedstoream_begin_index_fetch(Relation rel)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+
+static void
+zedstoream_reset_index_fetch(IndexFetchTableData* scan)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static void
+zedstoream_end_index_fetch(IndexFetchTableData* scan)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static bool
+zedstoream_fetch_follow(struct IndexFetchTableData *scan,
+					ItemPointer tid,
+					Snapshot snapshot,
+					TupleTableSlot *slot,
+					bool *call_again, bool *all_dead)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static bool
+zedstoream_scan_bitmap_pagescan(TableScanDesc sscan,
+							TBMIterateResult *tbmres)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static bool
+zedstoream_scan_bitmap_pagescan_next(TableScanDesc sscan, TupleTableSlot *slot)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static bool
+zedstoream_scan_sample_next_block(TableScanDesc sscan, struct SampleScanState *scanstate)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static bool
+zedstoream_scan_sample_next_tuple(TableScanDesc sscan, struct SampleScanState *scanstate, TupleTableSlot *slot)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static void
+zedstoream_scan_analyze_next_block(TableScanDesc sscan, BlockNumber blockno, BufferAccessStrategy bstrategy)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static bool
+zedstoream_scan_analyze_next_tuple(TableScanDesc sscan, TransactionId OldestXmin, double *liverows, double *deadrows, TupleTableSlot *slot)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static void
+zedstoream_relation_nontransactional_truncate(Relation rel)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static void
+zedstoream_set_new_filenode(Relation rel, char persistence,
+						TransactionId *freezeXid, MultiXactId *minmulti)
+{
+	/*
+	 * Initialize to the minimum XID that could put tuples in the table.
+	 * We know that no xacts older than RecentXmin are still running, so
+	 * that will do.
+	 */
+	*freezeXid = RecentXmin;
+
+	/*
+	 * Similarly, initialize the minimum Multixact to the first value that
+	 * could possibly be stored in tuples in the table.  Running
+	 * transactions could reuse values from their local cache, so we are
+	 * careful to consider all currently running multis.
+	 *
+	 * XXX this could be refined further, but is it worth the hassle?
+	 */
+	*minmulti = GetOldestMultiXactId();
+
+	RelationCreateStorage(rel->rd_node, persistence);
+
+	/*
+	 * If required, set up an init fork for an unlogged table so that it can
+	 * be correctly reinitialized on restart.  An immediate sync is required
+	 * even if the page has been logged, because the write did not go through
+	 * shared_buffers and therefore a concurrent checkpoint may have moved the
+	 * redo pointer past our xlog record.  Recovery may as well remove it
+	 * while replaying, for example, XLOG_DBASE_CREATE or XLOG_TBLSPC_CREATE
+	 * record. Therefore, logging is necessary even if wal_level=minimal.
+	 */
+	if (rel->rd_rel->relpersistence == RELPERSISTENCE_UNLOGGED)
+	{
+		Assert(rel->rd_rel->relkind == RELKIND_RELATION ||
+			   rel->rd_rel->relkind == RELKIND_MATVIEW ||
+			   rel->rd_rel->relkind == RELKIND_TOASTVALUE);
+		RelationOpenSmgr(rel);
+		smgrcreate(rel->rd_smgr, INIT_FORKNUM, false);
+		log_smgrcreate(&rel->rd_smgr->smgr_rnode.node, INIT_FORKNUM);
+		smgrimmedsync(rel->rd_smgr, INIT_FORKNUM);
+	}
+}
+
+static void
+zedstoream_relation_copy_data(Relation rel, RelFileNode newrnode)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static void
+zedstoream_estimate_rel_size(Relation rel, int32 *attr_widths,
+						 BlockNumber *pages, double *tuples,
+						 double *allvisfrac)
+{
+	ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("function not implemented yet")));
+}
+
+static const TableAmRoutine zedstoream_methods = {
+	.type = T_TableAmRoutine,
+
+	.slot_callbacks = zedstoream_slot_callbacks,
+
+	.tuple_satisfies_snapshot = zedstoream_tuple_satisfies_snapshot,
+
+//	.scan_begin = heap_beginscan,
+//	.scansetlimits = heap_setscanlimits,
+//	.scan_getnextslot = heap_getnextslot,
+//	.scan_end = heap_endscan,
+//	.scan_rescan = heap_rescan,
+//	.scan_update_snapshot = heap_update_snapshot,
+
+	.scan_bitmap_pagescan = zedstoream_scan_bitmap_pagescan,
+	.scan_bitmap_pagescan_next = zedstoream_scan_bitmap_pagescan_next,
+
+	.scan_sample_next_block = zedstoream_scan_sample_next_block,
+	.scan_sample_next_tuple = zedstoream_scan_sample_next_tuple,
+
+	.tuple_fetch_row_version = zedstoream_fetch_row_version,
+	.tuple_fetch_follow = zedstoream_fetch_follow,
+	.tuple_insert = zedstoream_insert,
+	.tuple_insert_speculative = zedstoream_insert_speculative,
+	.tuple_complete_speculative = zedstoream_complete_speculative,
+	.tuple_delete = zedstoream_delete,
+	.tuple_update = zedstoream_update,
+	.tuple_lock = zedstoream_lock_tuple,
+//	.multi_insert = heap_multi_insert,
+	.finish_bulk_insert = zedstoream_finish_bulk_insert,
+
+//	.tuple_get_latest_tid = heap_get_latest_tid,
+
+//	.relation_vacuum = heap_vacuum_rel,
+	.scan_analyze_next_block = zedstoream_scan_analyze_next_block,
+	.scan_analyze_next_tuple = zedstoream_scan_analyze_next_tuple,
+	.relation_nontransactional_truncate = zedstoream_relation_nontransactional_truncate,
+//	.relation_copy_for_cluster = heap_copy_for_cluster,
+	.relation_set_new_filenode = zedstoream_set_new_filenode,
+	.relation_copy_data = zedstoream_relation_copy_data,
+//	.relation_sync = heap_sync,
+	.relation_estimate_size = zedstoream_estimate_rel_size,
+
+	.begin_index_fetch = zedstoream_begin_index_fetch,
+	.reset_index_fetch = zedstoream_reset_index_fetch,
+	.end_index_fetch = zedstoream_end_index_fetch,
+
+//	.compute_xid_horizon_for_tuples = heap_compute_xid_horizon_for_tuples,
+
+//	.index_build_range_scan = IndexBuildHeapRangeScan,
+
+//	.index_validate_scan = validate_index_heapscan
+};
+
+Datum
+zedstore_tableam_handler(PG_FUNCTION_ARGS)
+{
+	PG_RETURN_POINTER(&zedstoream_methods);
+}
