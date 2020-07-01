5f97d3751f6bde4a2bba68a2db03f195bfce336f
Soumyadeep Chakraborty <sochakraborty@pivotal.io>
2020-07-01 13:55:38 -0700

    Pass all columns of rel in EPQ callsite for tuple_fetch_row_version()
    TODO: Use rti to index into the range table (possibly using
    exec_rt_fetch() and get scanCols. Then the scanCols can be passed into
    this callsite.
    
    Reviewed-by: Heikki Linnakangas <heikki.linnakangas@iki.fi>

--- src/backend/executor/execMain.c
+++ src/backend/executor/execMain.c
@@ -2647,7 +2647,8 @@ EvalPlanQualFetchRowMark(EPQState *epqstate, Index rti, TupleTableSlot *slot)
 			/* ordinary table, fetch the tuple */
 			if (!table_tuple_fetch_row_version(erm->relation,
 											   (ItemPointer) DatumGetPointer(datum),
-											   SnapshotAny, slot, NULL))
+											   SnapshotAny, slot,
+											   get_ordinal_attnos(erm->relation)))
 				elog(ERROR, "failed to fetch tuple for EvalPlanQual recheck");
 			return true;
 		}
